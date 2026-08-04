6e675d5e9076bd2989783b8c369a78c359c2d674
SoSource <sozed@email.com>
2026-08-03 20:47:08 -0400

    Update cronjobs.py

--- utils/cronjobs.py
+++ utils/cronjobs.py
@@ -67,17 +67,24 @@ def clear_chrome():
         prnt('clear chrome fail 5', str(e))
 
 
-def clear_jold_obs():
+def clear_old_jobs():
     import django_rq
     from rq.registry import FailedJobRegistry
 
-    failed_registry = FailedJobRegistry('main', connection=django_rq.get_connection('main'))
-    n = 0
-    for job_id in reversed(failed_registry.get_job_ids()):
-        n += 1
-        if n > 500:
-            try:
-                failed_registry.remove(job_id, delete_job=True)
-            except:
-                pass
+    queue_names = ['main', 'high', 'low', 'chat', 'super']
+
+    for queue_name in queue_names:
+        connection = django_rq.get_connection(queue_name)
+        failed_registry = FailedJobRegistry(queue_name, connection=connection)
+
+        n = 0
+        for job_id in reversed(failed_registry.get_job_ids()):
+            n += 1
+            if n > 500:
+                try:
+                    failed_registry.remove(job_id, delete_job=True)
+                except:
+                    # failed jobs expire in the queue. There's a
+                    # chance this will raise NoSuchJobError
+                    pass
 
