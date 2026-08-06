16fc9f2e0573bf4dee04c5cc6a8cc0b5b2779b2d
SoSource <sozed@email.com>
2026-08-06 19:07:41 -0400

    Update models.py

--- network/models.py
+++ network/models.py
@@ -4077,9 +4077,9 @@ class Blockchain(models.Model):
             if 'pending' not in self.queuedData:
                 self.queuedData['pending'] = {}
             if self.genesisId == transaction.ReceiverWallet_obj.id:
-                self.queuedData['pending'][transaction.id] = {'block':new_block.id,'index':new_block.index,'created':dt_to_string(transaction.created),'before':transaction.ReceiverWallet_obj.value,'value':transaction.token_value}
+                self.queuedData['pending'][transaction.id] = {'block':new_block.id,'index':new_block.index,'created':dt_to_string(transaction.created),'before':transaction.ReceiverWallet_obj.value,'value':str(transaction.token_value.token_value.normalize())}
             elif transaction.SenderWallet_obj and self.genesisId == transaction.SenderWallet_obj.id:
-                self.queuedData['pending'][transaction.id] = {'block':new_block.id,'index':new_block.index,'created':dt_to_string(transaction.created),'before':transaction.SenderWallet_obj.value,'value':f'-{transaction.token_value}'}
+                self.queuedData['pending'][transaction.id] = {'block':new_block.id,'index':new_block.index,'created':dt_to_string(transaction.created),'before':transaction.SenderWallet_obj.value,'value':f'-{str(transaction.token_value.token_value.normalize())}'}
             self.save()
             prnt('done create block')
 
