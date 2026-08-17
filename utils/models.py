3ea8db25939d6c516e02cb36ba89e4c91fcc1c88
SoSource <sozed@email.com>
2026-08-17 10:50:18 -0400

    Update models.py

--- utils/models.py
+++ utils/models.py
@@ -6585,7 +6585,7 @@ def process_received_data(received_data, block_dict=None, downstream_worker=True
                 elif updatedDB:
                     if obj._meta.object_name == 'Region': # ParentRegion_obj must be saved before bulk update
                         val_err += 'J'
-                        obj.save(sigs[0])
+                        obj.save(sigs)
                         save_sigs(sigs)
                     else:
                         bulk_update_items[obj.id] = {'is_new':is_new,'updatedDB':updatedDB,'obj':obj, 'sigs':sigs}
